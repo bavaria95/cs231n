Fix to compute gradients too

assignment2/cs231n/classifiers/fc_net.py
@@ -85,9 +85,10 @@ class TwoLayerNet(object):
         ############################################################################
         W1, b1 = self.params['W1'], self.params['b1']
         W2, b2 = self.params['W2'], self.params['b2']
-        N, D = X.shape
+        N = X.shape[0]
+        X_vec = X.reshape(N, -1)
 
-        h1 = X.dot(W1) + b1
+        h1 = X_vec.dot(W1) + b1
 
         h1 = np.maximum(0, h1) # ReLU
 
@@ -114,6 +115,19 @@ class TwoLayerNet(object):
         scores_norm = scores - np.max(scores, axis=1).reshape(-1, 1)
         loss =  np.average(-scores_norm[range(N), list(y)] + np.log(np.sum(np.exp(scores_norm), axis=1))) + \
                 0.5 * self.reg * (np.sum(W1 * W1) + np.sum(W2 * W2))
+
+        dscores = np.exp(scores_norm) / np.sum(np.exp(scores_norm), axis=1).reshape(-1, 1)
+        dscores[range(N), list(y)] -= 1
+        dscores /= N
+
+        grads['W2'] = h1.T.dot(dscores) + 2 * self.reg * W2
+        grads['b2'] = np.sum(dscores, axis=0)
+
+        dh1 = dscores.dot(W2.T)
+        dh1_relu = (h1 > 0) * dh1
+
+        grads['W1'] = X_vec.T.dot(dh1_relu) + 2 * self.reg * W1
+        grads['b1'] = np.sum(dh1_relu, axis=0)
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################