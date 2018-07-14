1f21a521cb5b226ba7242aae84a59f629689c9e8
Dmytro Petruk <bavaria95@gmail.com>
2018-07-14 18:35:56 +0300

    SGD with momentum

--- assignment2/cs231n/optim.py
+++ assignment2/cs231n/optim.py
@@ -65,11 +65,14 @@ def sgd_momentum(w, dw, config=None):
     # TODO: Implement the momentum update formula. Store the updated value in #
     # the next_w variable. You should also use and update the velocity v.     #
     ###########################################################################
-    pass
+    v = config['momentum'] * v - config['learning_rate'] * dw
+    w += v
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
     config['velocity'] = v
+    next_w = w
+
 
     return next_w, config
 
